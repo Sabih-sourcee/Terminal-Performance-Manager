9cdc3bda77c3d7765f69a668ce3e6614f8c3e923
bookspower218-lang <bookdpower218@gmail.com>
2026-07-24 01:39:33 +0500

    The colours and the RAM, Top process added

--- monitor.py
+++ monitor.py
@@ -1,11 +1,30 @@
 import psutil
-import os
-import time
+from rich.console import Console
+
+console = Console()
+
 while True:
-    cpu = psutil.cpu_percent(interval=1)
-    if os.name == 'nt':
-        clear_command = 'cls'
+    cpu = psutil.cpu_percent(interval=0.5)
+    ram = psutil.virtual_memory().percent
+    process = []
+    console.clear()
+    if cpu >= 80:
+        console.print(f"CPU Usage: {cpu}%", style="red")
+    elif cpu >= 50:
+        console.print(f"CPU Usage: {cpu}%", style="yellow")
     else:
-        clear_command = 'clear'
-    os.system(clear_command)
-    print(f'CPU Usage: {cpu}%')
+        console.print(f"CPU Usage: {cpu}%", style="green")
+
+    if ram >= 80:
+        console.print(f"RAM Usage: {ram}%", style="red")
+    elif ram >= 50:
+        console.print(f"RAM Usage: {ram}%", style="yellow")
+    else:
+        console.print(f"RAM Usage: {ram}%", style="green")
+    for proc in psutil.process_iter(['name', 'memory_percent']):
+        process.append(proc.info)
+    large_proce = process[0]
+    for proc in process:
+        if proc["memory_percent"] > large_proce["memory_percent"]:
+            biggest = proc
+    print(large_proce)
